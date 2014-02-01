610947750d5fcbd03bdd4595d87bf0481b182132
Michael Atlin <mike.atlin@gmail.com>
2014-02-01 15:39:44 -0500

    Restfull methods written, views and partials created, database seeded with entries.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+products = Product.create([
+  {name: "The Lion, the Witch, and the wardrobe", price_in_cents: 1000, description: "A heartwarming children's novel about orphans, Christianity, and animal cruelty."},
+  {name: "Railsea", price_in_cents: 899, description: "A rousing story full of sound and trains and giant moles, and signifying that China Mieville is better off when he doesn't let marxism completely take over his better judgement."},
+  {name: "Blender", price_in_cents: 8995, description: "We also sell blenders"},
+
+])
+
+# t.string   "name"
+#     t.text     "description"
+#     t.integer  "price_in_cents"
+#     t.datetime "created_at"
+#     t.date  tw  me "updated_at"e
