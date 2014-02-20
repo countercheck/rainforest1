36dd5d14e602ce082a53773dc29df6cda3976112
Michael Atlin <mike.atlin@gmail.com>
2014-02-20 18:29:56 -0500

    Infinite Scrolling implimented and cleaned up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,9 +18,20 @@
 $(document).ready(function(){
   $(window).scroll(function(){
     if ($(window).scrollTop() > $(document).height() - $(window).height() - 50) {
-      return alert('near bottom');
+      console.log($('.pagination span.next').children().attr('href'));
+      $.getScript($('.pagination span.next').children().attr('href'));
     };
   });
-
-
 });
+
+// $(document).ready(function(){
+//   if ($('.pagination').length) {
+//     $(window).scroll(function() {
+//       var url = $('.pagination span.next').children().attr('href');
+//       if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+//         $('.pagination').text("Fetching more products...");
+//         return $.getScript(url);
+//       }
+//     });
+//   }
+// });
